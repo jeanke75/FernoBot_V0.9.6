70bf0747e6e4a3e6a920da3c67f51d34aa36790b
tomjan <tomjan@HI71209>
2017-01-31 09:08:59 +0100

    fix program

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -525,7 +525,7 @@ namespace DiscordBot
                         else
                         {
                             await e.Channel.SendMessage("test");
-                            await e.Channel.SendMessage(await RPG_Controller.Inventory(e.Message.User, e.GetArg("Page"), e.Channel));
+                            await e.Channel.SendMessage(await RPG_Controller.Inventory(e.Message.User, e.GetArg("Page")));
                             await RPG_Controller.SetCooldown(e.Message.User, "inventory");
                         }
                     }
